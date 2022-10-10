37fe3601bf253afd9cfc55f7036902f90a492f30
V _VITHURSHAN <64639291+vvithurshan@users.noreply.github.com>
2022-10-10 14:16:50 +0530

    deletion of some credentials

--- PHASE-2/S1/FM-OCT-10-S1-V1/output/path.sh
+++ PHASE-2/S1/FM-OCT-10-S1-V1/output/path.sh
@@ -1,8 +1,8 @@
 rm -r HILLS_ROOT
 source /home/vvithurshan/Documents/scripts/password.sh
-dir=/home/nmrbox/0014/vvarenthirarajah/Documents/project_Meta/S1/PHASE-2/FM-OCT-10-S1-V1/HILLS_ROOT
+dir=/home/${server}/0014/vvarenthirarajah/Documents/project_Meta/S1/PHASE-2/FM-OCT-10-S1-V1/HILLS_ROOT
 dir2=/home/vvithurshan/Desktop/Research_22/PHASE-2/S1/FM-OCT-10-S1-V1/output
-scp -r vvarenthirarajah@tin.nmrbox.org:$dir $dir2
+scp -r vvarenthirarajah@${server}:$dir $dir2
 cd HILLS_ROOT
 cat HILLS.*| sort -n > HILLS
 plumed sum_hills --hills HILLS --stride 1000 --mintozero
